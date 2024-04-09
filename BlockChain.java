4372d77d4cf2d5f6a7495b425958d86b9735fcf4
VivienYan <158091529+VivienYan@users.noreply.github.com>
2024-04-08 19:55:31 -0500

    Update BlockChain.java

--- BlockChain.java
+++ BlockChain.java
@@ -1,3 +1,13 @@
 public class BlockChain {
   
+  Node2 first;
+  Node2 last;
+  int size;
+
+  BlockChain(int initial){
+    this.first = new Node2(new Block(0, initial, null));
+    this.last = first;
+    this.size = 1;
+  }
+
 }
