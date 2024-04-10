0f7e4f5418c9728a547ad328b10e406582d3a02d
trungle15 <50526463+trungle15@users.noreply.github.com>
2024-04-09 20:28:53 -0500

    update condition for isValid

--- Hash.java
+++ Hash.java
@@ -8,21 +8,25 @@ public class Hash {
   public Hash(byte[] data){
     this.hash = data;
   }
+
   public byte[] getData(){
     return this.hash;
   }
+
   public boolean isValid(){
-    return hash[0] == 0 && hash[1] == 0 && hash[2] == 0;
+    return hash.length >= 3 && hash[0] == 0 && hash[1] == 0 && hash[2] == 0;
   }
+
   public String toString(){
     String ret = new String();
 
-    for(byte r : hash){
+    for (byte r : hash) {
      ret = String.format(ret, Byte.toUnsignedInt(r));
     }
 
     return ret;
   }
+
   public boolean equals(Object other){
     if (other instanceof Hash){
       Hash o = (Hash) other;
