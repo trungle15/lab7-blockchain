7e2525e7655ef7f4d66f9bbc7d96a6aedfe8ef34
Yuxuan Li <tony_li@YuxuandeMacBook-Pro-3.local>
2024-04-09 21:24:15 -0500

    Add my amazing tests.

--- BlockChain.java
+++ BlockChain.java
@@ -5,7 +5,7 @@ public class BlockChain {
   int size;
 
   BlockChain(int initial){
-    this.first = new Node2(new Block(0, initial, null));
+    this.first = new Node2<Block>(new Block(0, initial, null));
     this.last = first;
     this.size = 1;
   }
@@ -38,4 +38,54 @@ public class BlockChain {
       return true;
     }
   }
+  public Hash getHash() {
+    return this.last.value.getHash();
+}
+
+public void printBalances() {
+  int alexisBalance = this.first.value.getAmount(); 
+  Node2<Block> current = this.first.next; 
+
+  while (current != null) {
+      alexisBalance += current.value.getAmount(); 
+      current = current.next;
+  }
+
+  int blakeBalance = -alexisBalance + this.first.value.getAmount();
+
+  System.out.println("Alexis: " + alexisBalance + ", Blake: " + blakeBalance);
+}
+
+public boolean isValidBlockChain() {
+  int alexisBalance = this.first.value.getAmount(); 
+  Node2<Block> current = this.first.next; 
+
+  while (current != null) {
+      Block block = current.value;
+
+      alexisBalance += block.getAmount();
+
+      if (alexisBalance < 0) {
+          return false;
+      }
+
+      if (!block.getPrevHash().equals(current.prev.value.getHash())) {
+          return false;
+      }
+      current = current.next;
+  }
+
+  return true;
+}
+
+public String toString() {
+  StringBuilder builder = new StringBuilder();
+  Node2<Block> current = this.first;
+  while (current != null) {
+      builder.append(current.value.toString()).append("\n");
+      current = current.next;
+  }
+  return builder.toString();
+}
+
 }
